d09ea8dfbd964b9dbb0e8482a50feef7fbf1c816
cosmo <cosmo@LAPTOP-R3LPCNKK>
2019-04-16 21:16:34 +0200

    da testare

--- BoulderDashDLV/src/graphic/PlayScene.java
+++ BoulderDashDLV/src/graphic/PlayScene.java
@@ -4,7 +4,9 @@ import interfaces.GameScene;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import model.World;
+import utility.Constants;
 import utility.Direction;
 
 public class PlayScene implements GameScene {
@@ -12,6 +14,7 @@ public class PlayScene implements GameScene {
 	protected SceneManager manager;
 	private World world;
 	private int level;
+	private boolean indietroOn = false; 
 	/*private PlayerObject player;
 	private ObstacleManager obManager;*/
 	
@@ -44,10 +47,41 @@ public class PlayScene implements GameScene {
 
 	public void draw() {
 		world.draw();
-		
+		if (indietroOn) {
+			Constants.context.drawImage(Constants.indietroplay_on, 19, 720);
+		} else {
+			Constants.context.drawImage(Constants.indietroplay, 19, 720);
+		}
 	}
 
 	public void handleEvent(Scene scene) {
+		scene.setOnMouseMoved(new EventHandler<MouseEvent>() {
+			@Override 
+			public void handle(MouseEvent arg0) {
+
+					if(arg0.getX() >= 19 && arg0.getX() <= (19 + Constants.indietroplay_on.getWidth()) 
+							&& arg0.getY() >= 720 && arg0.getY() <= (720 + Constants.indietroplay_on.getHeight())
+							&& manager.getGameScene() instanceof PlayScene) {
+						//manager.switchToPlay();
+						indietroOn = true;
+					} 
+					else {
+						indietroOn = false;
+					}
+			}
+		});
+		scene.setOnMousePressed(new EventHandler<MouseEvent>() {
+			@Override 
+			public void handle(MouseEvent arg0) {
+				if(arg0.isPrimaryButtonDown()) {
+					if(arg0.getX() >= 19 && arg0.getX() <= (19 + Constants.indietroplay_on.getWidth()) 
+							&& arg0.getY() >= 720 && arg0.getY() <= (720 + Constants.indietroplay_on.getHeight())
+							&& manager.getGameScene() instanceof PlayScene) {
+						manager.switchToLevels();
+					}
+				}
+			}
+		});
 		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
 			
 			public void handle(KeyEvent arg0) {
--- BoulderDashDLV/src/graphic/PlaySceneDLV.java
+++ BoulderDashDLV/src/graphic/PlaySceneDLV.java
@@ -11,12 +11,14 @@ import dlv_model.*;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import model.Diamond;
 import model.Empty;
 import model.Ground;
 import model.Stone;
 import model.Wall;
 import model.World;
+import utility.Constants;
 import utility.Direction;
 
 import it.unical.mat.embasp.base.Handler;
@@ -35,7 +37,8 @@ public class PlaySceneDLV implements GameScene {
 	protected SceneManager manager;
 	private World world;
 	private int level;
-
+	private boolean indietroOn = false;
+	
 	private static String encodingResource = "/home/paolo/git/BoulderDashDLV/BoulderDashDLV/src/res/encodings/rules";
 	private static Handler handler = new DesktopHandler(new DLV2DesktopService("lib/dlv2"));
 	InputProgram facts = new ASPInputProgram();
@@ -141,8 +144,40 @@ public class PlaySceneDLV implements GameScene {
 
 	public void draw() {
 		world.draw();
+		if (indietroOn) {
+			Constants.context.drawImage(Constants.indietroplay_on, 19, 720);
+		} else {
+			Constants.context.drawImage(Constants.indietroplay, 19, 720);
+		}
 	}
 
 	public void handleEvent(Scene scene) {
+		scene.setOnMouseMoved(new EventHandler<MouseEvent>() {
+			@Override 
+			public void handle(MouseEvent arg0) {
+
+					if(arg0.getX() >= 19 && arg0.getX() <= (19 + Constants.indietroplay_on.getWidth()) 
+							&& arg0.getY() >= 720 && arg0.getY() <= (720 + Constants.indietroplay_on.getHeight())
+							&& manager.getGameScene() instanceof PlayScene) {
+						//manager.switchToPlay();
+						indietroOn = true;
+					} 
+					else {
+						indietroOn = false;
+					}
+			}
+		});
+		scene.setOnMousePressed(new EventHandler<MouseEvent>() {
+			@Override 
+			public void handle(MouseEvent arg0) {
+				if(arg0.isPrimaryButtonDown()) {
+					if(arg0.getX() >= 19 && arg0.getX() <= (19 + Constants.indietroplay_on.getWidth()) 
+							&& arg0.getY() >= 720 && arg0.getY() <= (720 + Constants.indietroplay_on.getHeight())
+							&& manager.getGameScene() instanceof PlayScene) {
+						manager.switchToLevels();
+					}
+				}
+			}
+		});
 	}
 }
